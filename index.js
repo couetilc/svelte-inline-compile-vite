b238e90d9daa1af5b5edb32b1fcf9058e02fc7f4
Connor Couetil <couetilc@gmail.com>
2022-04-03 23:25:08 -0700

    store the code in the database

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 import crypto from 'crypto';
-import fs from 'fs-extra';
+import { mkdirSync } from 'fs';
 import url from 'url';
 import { join } from 'path';
 import sqlite from 'better-sqlite3';
 
 const data_directory = url.fileURLToPath(new URL('./.data/', import.meta.url));
-const files_directory = join(data_directory, 'files');
 
-fs.mkdirpSync(data_directory);
-fs.mkdirpSync(files_directory);
+mkdirSync(data_directory, { recursive: true });
 
 const db = sqlite(join(data_directory, 'plugin.db'));
 
 const create_table = db.prepare(`
   CREATE TABLE IF NOT EXISTS svelte_fragments (
-    path TEXT NOT NULL UNIQUE,
-    hash TEXT NOT NULL UNIQUE
+    hash TEXT NOT NULL UNIQUE,
+    code TEXT
   );
 `);
 
-create_table.run();
-
-const delete_table = db.prepare(`
-  DELETE FROM svelte_fragments WHERE true;
+const drop_table = db.prepare(`
+  DROP TABLE IF EXISTS svelte_fragments;
 `);
+
+drop_table.run();
+create_table.run();
 
 const create_index_hash = db.prepare(`
   CREATE INDEX IF NOT EXISTS idx_svelte_fragments_hash ON svelte_fragments(hash);
@@ -32,11 +31,11 @@ const create_index_hash = db.prepare(`
 create_index_hash.run();
 
 const insert_fragment = db.prepare(`
-  INSERT OR IGNORE INTO svelte_fragments (path, hash) VALUES (?, ?);
+  INSERT OR IGNORE INTO svelte_fragments (hash, code) VALUES (?, ?);
 `);
 
 const select_fragment = db.prepare(`
-  SELECT path FROM svelte_fragments WHERE hash = ?;
+  SELECT code FROM svelte_fragments WHERE hash = ?;
 `);
 
 const moduleId = 'virtual:inline-svelte:';
@@ -46,10 +45,8 @@ export function svelte(...code) {
   const fragment = code.join('');
   const hash = crypto.createHash('md5').update(fragment).digest('hex');
   const filename = hash + extension;
-  const path = join(files_directory, filename);
   const id = moduleId + filename;
-  fs.writeFileSync(path, fragment);
-  insert_fragment.run(path, hash)
+  insert_fragment.run(hash, fragment)
   return import(id);
 }
 
@@ -57,19 +54,18 @@ export function SvelteInlineCompile() {
   return {
     name: 'svelte-inline-compile',
 
-    resolveId(id) {
+    load(id) {
       if (id.startsWith(moduleId)) {
         const hash = id.replace(moduleId, '').replace(extension, '');
         const fragment = select_fragment.get(hash);
         if (fragment) {
-          return fragment.path;
+          return fragment.code;
         }
       }
     },
 
     closeBundle() {
-      fs.emptyDirSync(files_directory);
-      delete_table.run();
+      drop_table.run();
     },
   }
 }
